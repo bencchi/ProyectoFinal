964a51144d55850e9ef7f0473b7612203eef4310
Benjamín Racchi <benjaminracchii@gmail.com>
2025-04-28 01:18:31 -0300

    Se añadió la funcionalidad de login/logout con su autenticación correspondiente para el super usuario

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,14 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from principal import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", views.inicio, name="inicio"),
     path("portfolio-details/", views.portfolio, name="portfolio"),
     path("service-details/", views.service, name="service"),
-    path("starter-page/", views.starter, name="starter")
+    path("starter-page/", views.starter, name="starter"),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("panel-admin/", views.panel_administracion, name="panel_admin"),
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
 
 def inicio(request):
     return render(request, "index.html")
 
+
 def portfolio(request):
     return render(request, "portfolio-details.html")
+
 
 def service(request):
     return render(request, "service-details.html")
 
+
 def starter(request):
     return render(request, "starter-page.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
+@login_required
+def panel_administracion(request):
+    return render(request, "panel_admin.html")
